1d5c1c422e0d7adbffa8f19ccfe575feee682bc0
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-10-26 18:54:34 +0200

    fix

--- formatting.py
+++ formatting.py
@@ -1,10 +1,72 @@
-
 import numpy as np
-def the_abc_func(a,                    b,           c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z):
+
+
+def the_abc_func(
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
+    l,
+    m,
+    n,
+    o,
+    p,
+    q,
+    r,
+    s,
+    t,
+    u,
+    v,
+    w,
+    x,
+    y,
+    z,
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
+    l,
+    m,
+    n,
+    o,
+    p,
+    q,
+    r,
+    s,
+    t,
+    u,
+    v,
+    w,
+    x,
+    y,
+    z,
+):
     pass
 
-a_short_list = [
-"an element"
-]
 
-a_long_list = ["an element",  "an element",      "an element",  "an element",  "an element",  "an element", "an element",  "an element"]
+a_short_list = ["an element"]
+
+a_long_list = [
+    "an element",
+    "an element",
+    "an element",
+    "an element",
+    "an element",
+    "an element",
+    "an element",
+    "an element",
+]
